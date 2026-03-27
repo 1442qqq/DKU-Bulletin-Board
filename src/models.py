ab5aa2c1c023a08d487a6da5907d8c6df42c24ad
1442qqq <blue2u75@gmail.com>
2026-03-27 19:31:11 +0900

    User model defined

--- src/models.py
+++ src/models.py
@@ -2,9 +2,18 @@
 
 from database import db
 from datetime import datetime, timezone
+import enum
 
+class UserRole(enum.Enum):
+    USER="USER"
+    ADMIN="ADMIN"
 
+    
 class User(db.Model):
     __tablename__ = 'user' #실제 테이블 이름과 매칭
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
+    nickname = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(127), nullable=False)
+    role = db.Column(db.Enum(UserRole), nullable=False, default=UserRole.USER)
+    user_id = db.Column(db.String(20), unique=True, nullable=False)
